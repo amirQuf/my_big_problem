95e2db1d481e8716928fc8f128937f183f201436
amirQuf <amir.ghasemian.ag@gmail.com>
2025-12-16 20:25:54 +0330

    fix: fix ./requirements.txt

---
+++ test_mapper.py
@@ -0,0 +1,20 @@
+import pytest
+from keyboard_mapper import map_en_to_fa ,map_fa_to_en
+
+
+def test_map_fa_to_en():
+    fa_input  = "فثسف"
+    result  = map_fa_to_en(fa_input)
+    
+    assert  result == "test"
+    
+    
+    
+    
+
+def test_map_en_to_fa():
+    en_input = "jsj"
+    result = map_en_to_fa(en_input)
+    
+    assert result == "تست"    
+    
